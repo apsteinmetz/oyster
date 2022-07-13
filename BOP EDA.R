83ee99c2ce6613e242f7591e88eecb60b9eedfaf
Art Steinmetz <apsteinmetz@yahoo.com>
2022-07-13 17:43:31 -0400

    finished data wrangle

--- BOP EDA.R
+++ BOP EDA.R
@@ -10,30 +10,32 @@ wq_url <-
 wq_meta_raw <- read_sheet(wq_url,"Information",range = "A10:J400")
 wq_data_raw <- read_sheet(wq_url,"Data")
 
-wq_meta <- wq_meta_raw
-names(wq_meta) <- names(wq_meta) %>% str_replace_all(" ","_")
-names(wq_meta) <- names(wq_meta) %>% str_replace_all("/","_")
-names(wq_meta)[2] <- "Site"
-
-wq_meta <- wq_meta %>%
-  filter(!is.na(Site)) %>%
+wq_meta <- wq_meta_raw %>%
+  rename_with(~str_replace_all(.x," ","_")) %>%
+  rename_with(~str_replace_all(.x,"/","_")) %>%
+  rename("site" = 2) %>%
+  filter(!is.na(site)) %>%
   mutate(Currently_Testing = if_else(is.na(Currently_Testing),0,1)) %>%
-  mutate(Currently_Testing = as.logical(Currently_Testing))
+  mutate(Currently_Testing = as.logical(Currently_Testing)) %>%
+  rename_with(tolower) %>%
+  mutate(site = as.factor(site))
 
+
+data_names <- c("site","date","year","month","high_tide","sample_time","bacteria",
+                "precip_t0","precip_t-1","precip_t-2","precip_t-3","precip_t-4",
+                "precip_t-5","precip_t-6","notes")
 
 wq_data <- wq_data_raw
-names(wq_data) <- names(wq_data) %>% str_replace_all(" ","_")
-names(wq_data) <- names(wq_data) %>% str_replace_all("/","_")
-
-
-mash_datetime <- function(in_date,in_datetime){
-  date(in_datetime) <- date(in_date)
-  return(in_datetime)
-}
-
-wq_data %>%
-  mutate(Full_Date = as_date(Full_Date)) %>%
-  mutate(Sample_Time = hms::as_hms(Sample_Time)) %>%
-  mutate(Battery_High_Tide = hms::as_hms(Battery_High_Tide))
-
+names(wq_data) <- data_names
+wq_data <- wq_data %>%
+  mutate(date = as_date(date)) %>%
+  mutate(sample_time = hms::as_hms(sample_time)) %>%
+  mutate(high_tide = hms::as_hms(high_tide)) %>%
+  mutate(across(where(is.list),as.character)) %>%
+  mutate(across(.fns = ~str_replace(.x,"<10","0"))) %>%
+  mutate(across(.fns = ~str_replace(.x,"Trace","0"))) %>%
+  mutate(site = as.factor(site)) %>%
+  # fix some typos
+  mutate(site = str_replace(site,"Daylighted Section","daylighted section")) %>%
+  mutate(site = str_replace(site,"Govenors","Governors"))
 
