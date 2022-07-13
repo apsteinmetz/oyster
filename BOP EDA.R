8faed80f7d7872a9c641aaffa47cfac8045c1549
Art Steinmetz <apsteinmetz@yahoo.com>
2022-07-13 18:17:02 -0400

    more wrangling

--- BOP EDA.R
+++ BOP EDA.R
@@ -4,12 +4,14 @@ library(tidyverse)
 library(googlesheets4)
 library(lubridate)
 
+# Get Data ----------------------------------
 wq_url <-
 "https://docs.google.com/spreadsheets/d/1813b2nagaxZ80xRfyMZNNKySZOitro5Nt7W4E9WNQDA/edit?usp=sharing"
 
 wq_meta_raw <- read_sheet(wq_url,"Information",range = "A10:J400")
 wq_data_raw <- read_sheet(wq_url,"Data")
 
+# Wrangle Data ----------------------------------
 wq_meta <- wq_meta_raw %>%
   rename_with(~str_replace_all(.x," ","_")) %>%
   rename_with(~str_replace_all(.x,"/","_")) %>%
@@ -32,10 +34,24 @@ wq_data <- wq_data %>%
   mutate(sample_time = hms::as_hms(sample_time)) %>%
   mutate(high_tide = hms::as_hms(high_tide)) %>%
   mutate(across(where(is.list),as.character)) %>%
-  mutate(across(.fns = ~str_replace(.x,"<10","0"))) %>%
-  mutate(across(.fns = ~str_replace(.x,"Trace","0"))) %>%
+  mutate(across(where(is.character),.fns = ~str_replace(.x,"<10","0"))) %>%
+  # > 24196 test limit?
+  mutate(across(where(is.character),.fns = ~str_replace(.x,">",""))) %>%
+  mutate(across(where(is.character),.fns = ~str_replace(.x,"Trace","0"))) %>%
+  # get rid of snow inches next to precip as water
+  mutate(across(where(is.character),.fns = ~str_replace(.x,"\\(.+\\)",""))) %>%
+  mutate(across(where(is.character),.fns = ~na_if(.x,"N/A"))) %>%
+  mutate(across(contains("precip"),as.numeric)) %>%
+  mutate(bacteria=as.numeric(bacteria)) %>%
+  mutate(notes = replace_na(notes,"")) %>%
   mutate(site = as.factor(site)) %>%
   # fix some typos
   mutate(site = str_replace(site,"Daylighted Section","daylighted section")) %>%
   mutate(site = str_replace(site,"Govenors","Governors"))
+
+# precip summary
+
+temp <- wq_data %>%
+  group_by(site) %>%
+  summarise(avg_bacteria = round(mean(bacteria,na.rm =TRUE)))
 
