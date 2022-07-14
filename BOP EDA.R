b1d2d8f6dabfd9c87f23e7e0f29cb38faeff78f9
Art Steinmetz <apsteinmetz@yahoo.com>
2022-07-14 10:04:12 -0400

    quality buckets, refactoring

--- BOP EDA.R
+++ BOP EDA.R
@@ -33,25 +33,44 @@ wq_data <- wq_data %>%
   mutate(date = as_date(date)) %>%
   mutate(sample_time = hms::as_hms(sample_time)) %>%
   mutate(high_tide = hms::as_hms(high_tide)) %>%
-  mutate(across(where(is.list),as.character)) %>%
-  mutate(across(where(is.character),.fns = ~str_replace(.x,"<10","0"))) %>%
+  mutate(across(where(is.list), as.character)) %>%
+  mutate(across(where(is.character), .fns = ~ str_replace(.x, "<10", "0"))) %>%
   # > 24196 test limit?
-  mutate(across(where(is.character),.fns = ~str_replace(.x,">",""))) %>%
-  mutate(across(where(is.character),.fns = ~str_replace(.x,"Trace","0"))) %>%
+  mutate(across(where(is.character), .fns = ~ str_replace(.x, ">", ""))) %>%
+  mutate(across(where(is.character), .fns = ~ str_replace(.x, "Trace", "0"))) %>%
   # get rid of snow inches next to precip as water
-  mutate(across(where(is.character),.fns = ~str_replace(.x,"\\(.+\\)",""))) %>%
-  mutate(across(where(is.character),.fns = ~na_if(.x,"N/A"))) %>%
-  mutate(across(contains("precip"),as.numeric)) %>%
-  mutate(bacteria=as.numeric(bacteria)) %>%
-  mutate(notes = replace_na(notes,"")) %>%
+  mutate(across(where(is.character), .fns = ~ str_replace(.x, "\\(.+\\)", ""))) %>%
+  mutate(across(where(is.character), .fns = ~ na_if(.x, "N/A"))) %>%
+  mutate(across(contains("precip"), as.numeric)) %>%
+  mutate(bacteria = as.numeric(bacteria)) %>%
+  mutate(notes = replace_na(notes, "")) %>%
   mutate(site = as.factor(site)) %>%
   # fix some typos
-  mutate(site = str_replace(site,"Daylighted Section","daylighted section")) %>%
-  mutate(site = str_replace(site,"Govenors","Governors"))
+  mutate(site = str_replace(site, "Daylighted Section", "daylighted section")) %>%
+  mutate(site = str_replace(site, "Govenors", "Governors")) %>%
+  mutate(quality = cut(
+    bacteria,
+    breaks = c(-1, 0, 35, 104, 49999),
+    labels = c("Not Detected", "Good", "Fair", "Unacceptable")
+  ))
 
-# precip summary
 
-temp <- wq_data %>%
+# EDA ---------------------------------------------
+
+bacteria_by_site <- wq_data %>%
   group_by(site) %>%
-  summarise(avg_bacteria = round(mean(bacteria,na.rm =TRUE)))
+  summarise(bacteria = round(mean(bacteria,na.rm =TRUE)))
 
+bacteria_by_site <- wq_data %>%
+  group_by(site,quality) %>%
+  tally()
+
+
+bacteria_by_site %>%
+#  filter(bacteria < 500) %>%
+  ggplot(aes(bacteria)) + geom_histogram()
+
+wq_data %>%
+  filter(bacteria < 200) %>%
+  group_by(month) %>%
+  ggplot(aes(as.factor(month),bacteria)) + geom_boxplot()
