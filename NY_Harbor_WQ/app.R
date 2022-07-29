b30c434ea6197693787cb4b6f06aa3a68563ec06
Art Steinmetz <apsteinmetz@yahoo.com>
2022-07-29 13:34:47 -0400

    restored dates to slider

--- NY_Harbor_WQ/app.R
+++ NY_Harbor_WQ/app.R
@@ -3,16 +3,132 @@
 library(shiny)
 library(shinyWidgets)
 library(bslib)
-library(dplyr)
-library(stringr)
-library(ggplot2)
+library(tidyverse)
 library(lubridate)
 library(leaflet)
 library(RcppRoll)
+library(googlesheets4)
+library(rnoaa)
+
+
+
+# Get Data ----------------------------------
+UPDATE = FALSE
+if (UPDATE){
+  # Omit need for key since sheets are public
+  gs4_deauth()
+  wq_url <-
+    "https://docs.google.com/spreadsheets/d/1813b2nagaxZ80xRfyMZNNKySZOitro5Nt7W4E9WNQDA/edit?usp=sharing"
+
+  wq_meta_raw <- read_sheet(wq_url,"Information",range = "A10:J400")
+  wq_data_raw <- read_sheet(wq_url,"Data")
+
+  # Wrangle Data ----------------------------------
+  wq_meta <- wq_meta_raw %>%
+    rename_with(~str_replace_all(.x," ","_")) %>%
+    rename_with(~str_replace_all(.x,"/","_")) %>%
+    rename("site" = 2) %>%
+    filter(!is.na(site)) %>%
+    mutate(Currently_Testing = if_else(is.na(Currently_Testing),0,1)) %>%
+    mutate(Currently_Testing = as.logical(Currently_Testing)) %>%
+    rename_with(tolower) %>%
+    mutate(site = as.factor(site))
+
+
+  data_names <- c("site","date","year","month","high_tide","sample_time","bacteria",
+                  "precip_t-0","precip_t-1","precip_t-2","precip_t-3","precip_t-4",
+                  "precip_t-5","precip_t-6","notes")
+
+  wq_data <- wq_data_raw
+  names(wq_data) <- data_names
+  wq_data <- wq_data %>%
+    mutate(date = as_date(date)) %>%
+    mutate(sample_time = hms::as_hms(sample_time)) %>%
+    mutate(high_tide = hms::as_hms(high_tide)) %>%
+    mutate(across(where(is.list), as.character)) %>%
+    mutate(across(where(is.character), .fns = ~ str_replace(.x, "<10", "0"))) %>%
+    # > 24196 test limit?
+    mutate(across(where(is.character), .fns = ~ str_replace(.x, ">", ""))) %>%
+    mutate(across(where(is.character), .fns = ~ str_replace(.x, "Trace", "0"))) %>%
+    # get rid of snow inches next to precip as water
+    mutate(across(where(is.character), .fns = ~ str_replace(.x, "\\(.+\\)", ""))) %>%
+    mutate(across(where(is.character), .fns = ~ na_if(.x, "N/A"))) %>%
+    mutate(across(contains("precip"), as.numeric)) %>%
+    mutate(bacteria = as.numeric(bacteria)) %>%
+    mutate(notes = replace_na(notes, "")) %>%
+    # fix some typos
+    mutate(site = str_replace(site, "Daylighted Section", "daylighted section")) %>%
+    mutate(site = str_replace(site, "Govenors", "Governors")) %>%
+    mutate(quality = cut(
+      bacteria,
+      breaks = c(-1, 0, 35, 104, 49999),
+      labels = c("Not Detected", "Good", "Fair", "Unacceptable")
+    )) %>%
+    mutate(site = as.factor(site))
+
+  save(wq_data,file="www/wq_data.rdata")
+  save(wq_meta,file="www/wq_meta.rdata")
+
+}
 
 load("www/wq_data.rdata")
 load("www/wq_meta.rdata")
 load("www/weather.rdata")
+
+# Get latest weather
+datatypeids <- c("TMIN","TMAX","PRCP")
+
+# ncdc_datasets(stationid = "GHCND:USW00014732")
+
+# default is Laguardia airport nyc
+get_weather <-  function(station = "USW00014732",
+                         startdate = Sys.Date() - 365,
+                         enddate = Sys.Date() - 1,
+                         datatypeid = "TMAX") {
+  weather <- ncdc(
+    datasetid = 'GHCND',
+    stationid = paste0("GHCND:", station),
+    datatypeid=datatypeid,
+    startdate = startdate,
+    enddate = enddate,
+    limit = 1000,
+    add_units = TRUE
+  )
+  return(weather$data)
+}
+c_to_f <- function(temp) {
+  return(temp *9/5 + 32)
+}
+
+mm_to_in <- function(len) {
+  return(len*.039)
+}
+
+fix_raw_weather <- function(weather_raw) {
+  weather_raw %>%
+    transmute(date = as.Date(date),
+              station = "LGA",
+              datatype,
+              value) %>%
+    pivot_wider(names_from = "datatype",
+                values_from = "value") %>%
+    transmute(date,
+              station,
+              TEMP = c_to_f((TMIN + TMAX) / 20),
+              PRCP = mm_to_in(PRCP / 100))
+}
+
+if (UPDATE){
+  start_date <- max(weather$date) + 1
+  weather_update_raw <- map_dfr(datatypeids,
+                                function(d) get_weather(datatypeid = d,startdate = start_date))
+  if (nrow(weather_update_raw)>0){
+    weather_update <- fix_raw_weather(weather_update_raw)
+    weather <- bind_rows(weather,weather_update) %>% unique()
+  }
+}
+
+save(weather,file="www/weather.rdata")
 
 all_dates = unique(wq_data$date)
 weather <- weather %>%
@@ -128,6 +244,7 @@ server <- function(input, output, session) {
       labs(x = "") +
       scale_y_continuous(limits = c(0, max_rain))
   })
+
 
   output$wqPlot <- renderLeaflet({
     leaflet() %>%
