806c2d1a9f939db3365ea89363024442f4e401e8
Art Steinmetz <apsteinmetz@yahoo.com>
2024-08-05 14:53:30 -0400

    save weather history as google sheet

---
+++ R/write_weather_google_sheet.R
@@ -0,0 +1,20 @@
+# make weather spreadsheet
+library(tidyverse)
+library(duckplyr)
+library(googlesheets4)
+library(googledrive)
+
+tide_data <- df_from_parquet("data/tides_noaa.parquet") |>
+  select(-good_tide_station) |>
+  as_tibble()
+
+weather_data <- df_from_parquet("data/weather.parquet") |> as_tibble()
+
+## done once
+# ss <- gs4_create("weather_data_nyc")
+
+# save each data frame as a separate worksheet in a single google spreadsheet
+ss <- drive_get("weather_data_nyc")
+sheet_write(tide_data,ss,"tide_data")
+sheet_write(weather_data,ss,"weather_data")
+
