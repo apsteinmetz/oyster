133637039ba683f981e2bcfc08bebd7805dcba2e
Art Steinmetz <apsteinmetz@yahoo.com>
2024-08-05 14:53:16 -0400

    model variable prep

--- R/4 - prepare model variables.R
+++ R/4 - prepare model variables.R
@@ -11,10 +11,16 @@ library(progressr)
 # use duckdb for every tidyverse function
 methods_overwrite()
 
-wq_data <- df_from_parquet("data/wq_data.parquet") %>%
+wq_data <- df_from_parquet("data/wq_data.parquet")
+
+wq_data_select <- wq_data %>%
   select(bacteria,quality,site_id,date,precip_t0,precip_t1,precip_48)
 
-wq_meta <- df_from_parquet("data/wq_meta.parquet") %>%
+wq_meta <- df_from_parquet("data/wq_meta.parquet")
+# load station data
+precip_stations <- df_from_parquet("data/ghcnd_stations_ny.parquet")
+
+wq_meta_select <- wq_meta %>%
    select(
      site_id,
      site,
@@ -23,6 +29,28 @@ wq_meta <- df_from_parquet("data/wq_meta.parquet") %>%
      nys_dec_water_body_classification,
      nyc_dep_wrrf_or_sewershed
    )
+wq_meta_station_key <- df_from_parquet("data/wq_meta_station_key.parquet")
+
+station_conflicts <-
+  wq_meta_station_key %>%
+  select(site, site_id, tide_noaa_id, precip_ghcn_id) |>
+  left_join(
+    select(
+      wq_meta,
+      site_id,
+      water_body,
+      borough_state,
+      harmonic_noaa_tide_stations,
+      noaa_rainfall_stations_id
+    ),
+    by = c("site_id")
+  ) |>
+  as_tibble() |>
+  mutate(
+    tides_agree = tide_noaa_id == harmonic_noaa_tide_stations,
+    precip_agree = precip_ghcn_id == noaa_rainfall_stations_id
+  )
+station_conflicts
 
 tide_data <- df_from_parquet("data/wq_tide_data.parquet") %>%
   mutate(.before=1,date = as.Date(sample_time)) %>%
@@ -43,26 +71,25 @@ model_data <- wq_data %>%
   # change chr to factor
   mutate(across(where(is.character),as.factor)) %>%
   # change site back to character
-  mutate(site = as.character(site))
-model_data
+  mutate(site = as.character(site)) |>
+  rename(bop_precip_t0 = precip_t0,
+         bop_precip_t1 = precip_t1,
+         bop_precip_48 = precip_48,
+         ghcn_precip_in = precip_in,
+         ghcn_precip_in_48 = precip_in_48)
+model_data |> as_tibble()
 
 # save model_data
 write_parquet(model_data,"data/model_data.parquet")
 
 # a lot of disagreement on precipitation
+# look at LGA alone
 model_data %>%
-  ggplot(aes(x=precip_in_48,y=precip_48)) +
+  filter(site_id == "15") |>
+  ggplot(aes(x=ghcn_precip_in,y=bop_precip_t0)) +
   geom_point() +
-  labs(title="Scatter plot of precip_in vs precip_in_48",
-       x="ghc Precipitation in 48 hours (in)",
-       y="bop Precipitation in 48 hours (in)")
+  geom_smooth(method="lm") +
+  labs(title="Scatter plot of BOP vs GHCN precipitation")
 
 
 
-# scatter plot of precip_t1 vs precip_in
-model_data %>%
-  ggplot(aes(x=precip_t0,y=precip_in)) +
-  geom_point() +
-  labs(title="Scatter plot of precip_t1 vs precip_in",
-       x="Precipitation BOP",
-       y="Precipitation ghcn")
